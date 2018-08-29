cfefd6e1487aae0a0ad74c957e1bc5a7c8eb8668
Sushi Chanrai <sushi@pathworks.co>
2018-08-29 16:50:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,29 +9,15 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last #turn /items/Thing into Thing
-      item = @@items.find{|i| i.name == item_name}
-      resp.write item.artist
-    else 
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
+       if item = @@items.find{|i| i.name == item_name}
+        resp.write item.artist
+      else 
+        resp.write "Item not found"
+        resp.status = 404
       end
-    elsif req.path.match(/add/)
-      item_value = req.params["item"]
-        if @@items.include? item_value
-          @@cart << item_value
-          resp.write "added #{item_value}"
-        else
-          resp.write "We don't have that item!"
-        end
-    else
-      resp.write "Path Not Found"
+    else 
+        resp.write "Route not found"
+        resp.status = 404
     end
 
     resp.finish
